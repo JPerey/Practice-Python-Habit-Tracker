3fbb7bc30312dcfb1141a2c2c4420207b2e3548b
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-11-09 14:42:28 -0800

    UPDATE: added post changes and testing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
+import datetime
+import os
 
-TOKEN = "fluent98765"
+TOKEN = os.environ.get("TOKEN")
 USERNAME = "jamjam1990"
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -24,9 +26,30 @@ graphs_params = {
     "color": "sora",
 }
 
+# headers = {
+#     "X-USER-TOKEN": TOKEN,
+# }
+
+# graph_response = requests.post(url=graph_endpoint, json=graphs_params, headers=headers)
+# print(graph_response.text)
+
+# Posting a value to the graph
+
+pixel_post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
+current = datetime.datetime.now()
+
 headers = {
     "X-USER-TOKEN": TOKEN,
 }
 
-graph_response = requests.post(url=graph_endpoint, json=graphs_params, headers=headers)
-print(graph_response.text)
+pixel_post_params = {
+    "date": current.strftime("%Y%m%d"),
+    "quantity": "1",
+
+}
+
+print(current)
+print(current.strftime("%Y%m%d"))
+
+# pixel_post_response = requests.post(url=pixel_post_endpoint, json=pixel_post_params, headers=headers)
+# print(pixel_post_response.text)
